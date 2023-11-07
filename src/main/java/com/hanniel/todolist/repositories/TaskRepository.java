38b2c71ea0d257a90829d7dbb1436330ecc34828
hanniel09 <hannielvieira1227@gmail.com>
2023-11-06 21:33:01 -0300

    feat: create TaskRepository

---
+++ src/main/java/com/hanniel/todolist/repositories/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.hanniel.todolist.repositories;
+
+import com.hanniel.todolist.domain.tasks.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
