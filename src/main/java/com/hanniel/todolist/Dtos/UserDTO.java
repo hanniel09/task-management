e9dcab808cf68ac7e90a68da4513e34c305f7faa
hanniel09 <hannielvieira1227@gmail.com>
2023-11-08 22:13:40 -0300

    feat: create User

---
+++ src/main/java/com/hanniel/todolist/Dtos/UserDTO.java
@@ -0,0 +1,4 @@
+package com.hanniel.todolist.Dtos;
+
+public record UserDTO(String name, String password) {
+}
---
+++ src/main/java/com/hanniel/todolist/domain/user/User.java
@@ -0,0 +1,53 @@
+package com.hanniel.todolist.domain.user;
+
+import com.hanniel.todolist.Dtos.UserDTO;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(UserDTO data) {
+        this.name = data.name();
+        this.password = data.password();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
