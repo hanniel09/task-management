82473b52374c0083c9d7ec838d835f097be4b27e
hanniel09 <hannielvieira1227@gmail.com>
2023-11-09 17:29:36 -0300

    feat: create UserService and methods

---
+++ src/main/java/com/hanniel/todolist/services/UserService.java
@@ -0,0 +1,41 @@
+package com.hanniel.todolist.services;
+
+import com.hanniel.todolist.Dtos.UserDTO;
+import com.hanniel.todolist.domain.user.User;
+import com.hanniel.todolist.exceptions.RecordNotFoundException;
+import com.hanniel.todolist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+    }
+
+    public User createUser(UserDTO data) {
+        User user = new User(data);
+        return userRepository.save(user);
+    }
+
+    public User updateUser(UserDTO data, Long id) {
+        User savedUser = getUserById(id);
+        User user = new User(data);
+        user.setId(savedUser.getId());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
