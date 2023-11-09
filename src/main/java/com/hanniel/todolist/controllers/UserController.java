2af499ed3f7b7bb7510f1c9617480407eef4de3e
hanniel09 <hannielvieira1227@gmail.com>
2023-11-09 17:52:34 -0300

    feat: create UserController and methods

---
+++ src/main/java/com/hanniel/todolist/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.hanniel.todolist.controllers;
+
+import com.hanniel.todolist.Dtos.UserDTO;
+import com.hanniel.todolist.domain.user.User;
+import com.hanniel.todolist.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
+        return new ResponseEntity<>(userService.createUser(userDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody UserDTO userDTO, @PathVariable Long id){
+        return new ResponseEntity<>(userService.updateUser(userDTO, id), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
